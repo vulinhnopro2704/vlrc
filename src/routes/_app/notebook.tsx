a04944da18d750e04fdb45c50991867ab27cb92d
Linh Vũ Trương <lvtruong@LINHTMAC.mgm-edv.de>
2026-05-31 16:58:45 +0700

    fix: add explicit lazyLoad import in notebook route (routes excluded from auto-imports)

--- src/routes/_app/notebook.tsx
+++ src/routes/_app/notebook.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { lazyLoad } from '@platform-core/components';
 
 const UserNotebookPage = lazyLoad(() =>
   import('@/pages/UserNotebookPage').then(mod => ({ default: mod.UserNotebookPage }))
